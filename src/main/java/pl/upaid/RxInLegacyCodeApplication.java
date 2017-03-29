bb5ee40b046552104574d9437617f5f7559d1e46
lguz <lukasz.guz@upaid.pl>
2017-03-29 11:25:38 +0200

    Start project

---
+++ src/main/java/pl/upaid/RxInLegacyCodeApplication.java
@@ -0,0 +1,12 @@
+package pl.upaid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RxInLegacyCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RxInLegacyCodeApplication.class, args);
+	}
+}
