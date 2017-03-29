66c8e7a51cba794f45cffaf963ee3404ba4d549a
lguz <lukasz.guz@upaid.pl>
2017-03-29 20:03:58 +0200

    DeferredResult in controller

--- src/main/java/pl/upaid/domain/model/loan/BankChecker.java
+++ src/main/java/pl/upaid/domain/model/loan/BankChecker.java
@@ -1,13 +1,16 @@
 package pl.upaid.domain.model.loan;
 
+import lombok.extern.slf4j.Slf4j;
 import pl.upaid.domain.model.Sleeper;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static pl.upaid.domain.model.loan.CheckerResponse.OK;
 
+@Slf4j
 public class BankChecker implements LoanChecker {
 
     public CheckerResponse check(String client) {
+        log.info("Bank checking...");
         Sleeper.sleep(1, SECONDS);
         return OK;
     }
--- src/main/java/pl/upaid/domain/model/loan/BikChecker.java
+++ src/main/java/pl/upaid/domain/model/loan/BikChecker.java
@@ -1,14 +1,17 @@
 package pl.upaid.domain.model.loan;
 
+import lombok.extern.slf4j.Slf4j;
 import pl.upaid.domain.model.Sleeper;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static pl.upaid.domain.model.loan.CheckerResponse.REJECTED;
 
+@Slf4j
 public class BikChecker implements LoanChecker {
 
     public CheckerResponse check(String client) {
-        Sleeper.sleep(4, SECONDS);
+        log.info("BIK checking...");
+        Sleeper.sleep(2, SECONDS);
         return REJECTED;
     }
 }
